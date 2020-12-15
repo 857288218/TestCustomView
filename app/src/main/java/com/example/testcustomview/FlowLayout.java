c266b006245583cb827b9d1eba872f9cf3497d33
renjunqing <renjunqing@windimg.com>
2020-12-15 16:32:23 +0800

    add FlowLayout

---
+++ app/src/main/java/com/example/testcustomview/FlowLayout.java
@@ -0,0 +1,77 @@
+package com.example.testcustomview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FlowLayout extends ViewGroup {
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measureWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int measureHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int measureWidthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int measureHeightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        int lineHeight = 0, lightWidth = 0, height = 0, width = 0;
+        for (int i=0; i<getChildCount(); i++) {
+            View childView = getChildAt(i);
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+            MarginLayoutParams lp = (MarginLayoutParams) getLayoutParams();
+            int childWidth = childView.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = childView.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+            if (lightWidth + childWidth > measureWidth) {
+                //换行
+                height += lineHeight;
+                width = Math.max(lightWidth, width);
+
+                lightWidth = childWidth;
+                lineHeight = childHeight;
+            } else {
+                lineHeight = Math.max(childHeight, lineHeight);
+                lightWidth += childWidth;
+            }
+            if (i == getChildCount() - 1) {
+                height += lineHeight;
+                width = Math.max(lightWidth, width);
+            }
+        }
+        setMeasuredDimension(measureWidthMode == MeasureSpec.EXACTLY ? measureWidth : width, measureHeightMode == MeasureSpec.EXACTLY ? measureHeight : height);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int lineWidth = 0, lineHeight = 0;
+        int top = 0, left = 0;
+        for (int i=0; i<getChildCount(); i++) {
+            View child = getChildAt(i);
+            MarginLayoutParams lp = (MarginLayoutParams) getLayoutParams();
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+            if (lineWidth + childWidth > getMeasuredWidth()) {
+                //换行
+                top += lineHeight;
+                left = 0;
+                lineHeight = childHeight;
+                lineWidth = childWidth;
+            } else {
+                lineHeight = Math.max(lineHeight, childHeight);
+                lineWidth += childWidth;
+            }
+            int lc = left + lp.leftMargin;
+            int tc = top + lp.topMargin;
+            int rc = lc + child.getMeasuredWidth();
+            int bc = tc + child.getMeasuredHeight();
+            child.layout(lc, tc, rc, bc);
+            left += childWidth;
+        }
+    }
+}
