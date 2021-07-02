2c225d426ba13ca6826176494da936f38bd70a46
renjunqing <renjunqing@douban.com>
2021-07-02 10:18:08 +0800

    add androidx.fragment

---
+++ app/src/main/java/com/example/testcustomview/TestFragment.kt
@@ -0,0 +1,23 @@
+package com.example.testcustomview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+
+class TestFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setFragmentResultListener("result_key") { resultKey, bundle ->
+
+        }
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
