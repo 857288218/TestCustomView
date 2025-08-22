ac4424ad3dd3b39d6f31015ab61bccf3d51296bd
任俊清 <857288218@qq.com>
2025-08-22 14:14:55 +0800

    test

--- app/src/main/java/com/example/testcustomview/MyApplication.kt
+++ app/src/main/java/com/example/testcustomview/MyApplication.kt
@@ -1,15 +1,33 @@
 package com.example.testcustomview
 
 import android.app.Application
+import android.view.View
+import android.view.View.OnClickListener
+import com.example.testcustomview.mianshi.ByteDance
 
 class MyApplication : Application() {
+
+    var area: Int = 0
+
     override fun onCreate() {
         super.onCreate()
+        ByteDance().test()
         Thread.sleep(111)
 //        thread200()
     }
 
     private fun thread200() {
         Thread.sleep(200)
+    }
+
+    var shape = { a: String ->
+        var name: String = ""
+        fun a() {
+            name = "ss"
+        }
+    }
+
+    fun test(s: (Int) -> String) {
+
     }
 }
